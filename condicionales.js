f0cf894d088184fc2c5be804dce94984e4126795
jose20ricardo <jgricardopay@gmail.com>
2018-04-20 20:56:29 -0500

    hoy hice condicionales

---
+++ condicionales.js
@@ -0,0 +1,33 @@
+let edad=1300
+
+if(edad>=18){
+     console.log("Ya es mayor de edad");    
+     if(edad>=40 & edad<=60){
+     console.log("Podria ser tu jefe");
+     }
+    else if(edad>60 & edad<=90){
+     console.log("Podria ser tu abuelo");
+     }
+     if(edad>90 & edad<=1400){
+         console.log("Ya es una momia");
+     }
+}     
+if(edad>8 & edad<=12){
+    console.log("Es un morro");
+}
+if(edad>15 & edad<=18){
+    console.log("Va en prepa");
+}    
+if(edad>12 & edad<=15){ 
+ console.log("Esta en secundaria");
+}
+if(edad>6 & edad<=8){      
+console.log("Esta en la primaria");
+}
+if(edad>3 & edad<=6){
+    console.log("Va en kinder");
+}
+if(edad>0 & edad<=3){
+    console.log("Es un bebito")
+}
+
