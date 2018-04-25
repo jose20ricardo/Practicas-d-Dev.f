3c9b4435409e0a7e2089294a0b4ba88186955b75
jose20ricardo <jgricardopay@gmail.com>
2018-04-24 21:26:07 -0500

    este es el js del documento anterior

---
+++ html-hs.js
@@ -0,0 +1,28 @@
+let h1 = document.createElement("h1");
+let texto_h1= document.createTextNode("en que hora sale el sol?");
+h1.appendChild(texto_h1);
+
+ let elbody= document.getElementById("elbody");
+  elbody.appendChild(h1);
+
+let cinata_blanca = [
+    {
+        "nombre":"Omar",
+        "edad":24
+    },
+    {
+        "nombre" : "Nora",
+        "edad" :24
+    },
+    {
+        "nombre" : "Jose",
+        "edad" :14
+
+    }
+]
+for(i=0;i<cinata_blanca.length;i++){
+     let h1=document.createElement("h1");
+     let texto=document.createTextNode("este es"+cinata_blanca[i].nombre+"y tiene"+cinata_blanca[i].edad+"años")
+     h1.appendChild(texto);
+     elbody.appendChild(h1);
+}
